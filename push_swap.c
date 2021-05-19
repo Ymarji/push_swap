4c64fa6d4de7e7a09e158a59eb573613fafbd312
Youssef Marji <ymarji@e2r3p5.1337.ma>
2021-05-19 13:04:59 +0100

    start

--- push_swap.c
+++ push_swap.c
@@ -1,7 +1,23 @@
 
 #include "push_swap.h"
 
-void	data_collect(t_var *var,char **arg)
+int	ft_isdigit_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	data_collect(t_var *var,char **arg)
 {
 	char	**tab;
 	int		i;
@@ -16,12 +32,15 @@ void	data_collect(t_var *var,char **arg)
 		tab = ft_split(arg[i], ' ');
 		while (tab[j])
 		{
+			if (ft_isdigit_str(tab[j]) == 0)
+				return (0);
 			new = ft_lstnew_m(ft_atoi(tab[j]));
 			ft_lstadd_back_m(&(var->head), new);
 			j++;
 		}
 		i++;
 	}
+	return (1);
 }
 
 int	main(int ac, char **av)
@@ -32,7 +51,8 @@ int	main(int ac, char **av)
 	var = (t_var *)malloc(sizeof(t_var));
 	if (ac <= 1)
 		return (0);
-	data_collect(var, av);
+	if (data_collect(var, av) == 0)
+		ft_putendl_fd("Error", 1);
 	head = var->head;
 	while (head)
 	{
